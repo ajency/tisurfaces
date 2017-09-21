c1b8745398e959a5b4563d65c2c9a4d37baaf70b
jeromie <jeromie@ajency.in>
2017-09-21 18:08:38 +0530

    gst note

--- wp-content/themes/uncode/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ wp-content/themes/uncode/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -17,6 +17,7 @@ global $product;
 	<?php if ( ! $product->is_sold_individually() ) : ?>
 		<?php woocommerce_quantity_input( array( 'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : 1 ) ); ?>
 	<?php endif; ?>
+	<p class="gst-note">Note: 28% GST will be added at checkout</p>
 	<button type="submit" class="single_add_to_cart_button button alt btn btn-default"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 	<input type="hidden" name="add-to-cart" value="<?php echo absint( $product->id ); ?>" />
 	<input type="hidden" name="product_id" value="<?php echo absint( $product->id ); ?>" />
