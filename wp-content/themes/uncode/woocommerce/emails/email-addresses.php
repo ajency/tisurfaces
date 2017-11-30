aa7e181476a8aecac60124a7fff2d6e9e96d2ee9
jeromie <jeromie@ajency.in>
2017-11-30 19:02:29 +0530

    billing address changs

--- wp-content/themes/uncode/woocommerce/emails/email-addresses.php
+++ wp-content/themes/uncode/woocommerce/emails/email-addresses.php
@@ -24,10 +24,32 @@ $text_align = is_rtl() ? 'right' : 'left';
 
 ?><table id="addresses" cellspacing="0" cellpadding="0" style="width: 100%; vertical-align: top;" border="0">
   <tr>
-    <td class="td" style="text-align:<?php echo $text_align; ?>; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" valign="top" width="50%">
-      <h3><?php _e( 'Billing address', 'woocommerce' ); ?></h3>
+    <td class="td billing-td" style="background: #f7f7f7;text-align:<?php echo $text_align; ?>; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" valign="top" width="50%">
+      <h3><?php _e( 'Customer Details', 'woocommerce' ); ?></h3>
 
-      <p class="text"><?php echo $order->get_formatted_billing_address(); ?></p>
+      <table style="width:100%;">
+        <tr>
+          <td>
+            <p class="text"><?php echo $order->get_formatted_billing_address(); ?></p>
+          </td>
+          <td>
+            <label style="font-weight: 600; font-size: 12px;">Email: </label>
+            <p class="text">
+              <?php echo $order->get_billing_email(); ?>
+            </p>
+          </td>
+          <td>
+            <label style="font-weight: 600; font-size: 12px;">Phone: </label>
+            <p class="text">
+              <?php echo $order->get_billing_phone(); ?>
+            </p>
+          </td>
+        </tr>
+      </table>
+
+
+
+
     </td>
     <?php if ( ! wc_ship_to_billing_address_only() && $order->needs_shipping_address() && ( $shipping = $order->get_formatted_shipping_address() ) ) : ?>
       <td class="td" style="text-align:<?php echo $text_align; ?>; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" valign="top" width="50%">
